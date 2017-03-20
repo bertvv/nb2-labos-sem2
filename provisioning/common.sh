e9b013659793397ae3deda04a3361a11e9fe6839
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2017-03-20 19:16:33 +0100

    Setup admin user

--- provisioning/common.sh
+++ provisioning/common.sh
@@ -20,50 +20,45 @@ set -o pipefail
 # Name of the admin user. Exporting makes it available in the server specific
 # scripts.
 export readonly ADMIN_USER=bert
+readonly admin_passwd='$6$9hUev.agoxZru1Aa$JHhPWJyymtrdTQjsXZuLzsJqsmxhZLJUOVSpsGCbhB52MMqpLGjYlW7oyWRkcHSgVnvwRvqdsaSbedAt0Wk.90'
 
-#------------------------------------------------------------------------------
-# Ensure machine id is created
-#------------------------------------------------------------------------------
-# Journald needs the file /etc/machine-id in order to work. This checks if that
-# file exists, and initialises it if necessary
-if [ ! -f /etc/machine-id ]; then
-  info 'Generating machine ID'
-  systemd-machine-id-setup
-fi
+readonly public_key='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoJcKFIfhaDejJVlNJof6UVJHkTlFwZ7UByQ4DCAwve33oAsDIZomTS4aPWLlBOFczIlUsFIfFFu0vo2kXt23uI433qXLIZcpNe7R0gbS6z7CljkELGghgpXMnI7apMwtIt2hfpz+ddOwu1GRPD+gr/7IOPRHaH2dehq/tf3LT5qOsr07g3qQWbqgzM6QuClRONh+BPqRXMRgyZZbtoubImGlBrmYMaBFlkL7/7SXn0JP2d6MtSoRwYGyz0zUJKStnrsVey3cZ/P1h4XyQ5JF2CG7BIIzlN6LoH7BeNKake1IX8UZ8gvvc1nGQkaH/yezCDGQIYWIrcSX5uV+53K6N bert@emrakul'
+readonly dot_ssh="/home/${ADMIN_USER}/.ssh"
+readonly keys="${dot_ssh}/authorized_keys"
+
 
 #------------------------------------------------------------------------------
 # Package installation
 #------------------------------------------------------------------------------
-# TODO: install packages required on all servers
 
 info Starting common tasks
-
-info Restarting network interfaces
-systemctl restart network
-
 info Installating common packages
+
+yum install -y epel-release
+yum install -y bind-utils git nano tree vim-enhanced
 
 #------------------------------------------------------------------------------
 # Admin user
 #------------------------------------------------------------------------------
-# TODO: set up a user account that you can use to log in to the server. Ensure
-# your user can login with ssh without having to enter a password (using a key
-# pair).
 
 info Setting up admin user account
 ensure_user_exists "${ADMIN_USER}"
 
-info "Setting up passwordless SSH login"
+usermod --password="${admin_passwd}" "${ADMIN_USER}"
 
-ssh_dir="/home/${ADMIN_USER}/.ssh"
+assign_groups "${ADMIN_USER}" wheel
 
-if [ ! -d "${ssh_dir}" ]; then
-  mkdir "${ssh_dir}"
+# Copy public key
+
+if [ ! -d "${dot_ssh}" ]; then
+  info 'Creating .ssh directory'
+  mkdir "${dot_ssh}"
+  chown ${ADMIN_USER}:${ADMIN_USER} "${dot_ssh}"
+  chmod u=rwx,go= "${dot_ssh}"
 fi
-chown "${ADMIN_USER}:${ADMIN_USER}" "${ssh_dir}"
-chmod 700 "${ssh_dir}"
 
-cp /vagrant/provisioning/files/authorized_keys "${ssh_dir}"
-chown "${ADMIN_USER}:${ADMIN_USER}" "${ssh_dir}/authorized_keys"
-chmod 600 "${ssh_dir}/authorized_keys"
+info 'Copying public key'
+echo "${public_key}" > "${keys}"
+chown ${ADMIN_USER}:${ADMIN_USER} "${keys}"
+chmod u=rw,go= "${keys}"
 
